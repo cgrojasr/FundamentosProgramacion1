87f9948cadc3f1dd1214e6438e8c68f739c5bab1
Christian Rojas <pciscroj@upc.edu.pe>
2023-06-17 11:49:20 -0500

    Ejericios de la semana 5

--- src/Semana4/Ejercicio6.java
+++ src/Semana4/Ejercicio6.java
@@ -12,5 +12,37 @@ public class Ejercicio6 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de medicina: ");
+        double cantidad_medicina = scanner.nextDouble();
+        System.out.println("Ingresar la cantidad de días que pasará");
+        int cantidad_dias = scanner.nextInt();
+
+        double resto_medicina = RestoMedicina(cantidad_medicina, cantidad_dias);
+        System.out.println("En " + cantidad_dias + " días quedara " + resto_medicina + " unidades");
+
+        System.out.println("Ingresar el % de medicina a eliminar");
+        double porcentaje_esperado = scanner.nextDouble();
+
+        double cantidad_dias_eliminado = EliminadoDias(porcentaje_esperado/100);
+        System.out.println("El porcentaje de  " + porcentaje_esperado + " se cumplira en " + cantidad_dias_eliminado + " días");
+    }
+
+    static double RestoMedicina(double cantidad_medicina, int cantidad_dias){
+        double porcentaje = 0.2;
+        for(int i=0; i<cantidad_dias; i++){
+            cantidad_medicina = cantidad_medicina * (1-porcentaje);
+        }
+        return  cantidad_medicina;
+    }
+
+    static int EliminadoDias(double porcentaje_esperado){
+        double porcentaje = 1;
+        int cantidad_dias = 0;
+        while(porcentaje > porcentaje_esperado){
+            porcentaje = porcentaje * 0.8;
+            cantidad_dias++;
+        }
+        return cantidad_dias;
     }
 }
